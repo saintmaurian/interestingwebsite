e7d179fe38c64a413459fcbf0ee9c418522b2a3a
PunThePancake <71299502+PunThePancake@users.noreply.github.com>
2023-05-30 21:07:20 +1000

    Update lol-lmao.js

--- lol-lmao.js
+++ lol-lmao.js
@@ -1,2 +1,23 @@
 var audio = new Audio('most.mp3');
 audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
+
+var audio = new Audio('most.mp3');
+audio.play();
